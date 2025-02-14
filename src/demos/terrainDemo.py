eed1443d5eba2fab8ff83bf1f09fd1efc7c46b57
Dylan Green <cobaltgoldcsprinks@gmail.com>
2025-02-14 12:17:19 -0700

    Vehicle visualized
    The only thing to do now is to visualize the terrain and make it
    somewhat workable.

--- src/demos/terrainDemo.py
+++ src/demos/terrainDemo.py
@@ -1,29 +1,53 @@
-from pychrono.core import ChSystemNSC, ChVector3d, ChCoordsysd
+from pychrono.core import ChVector3d, ChCoordsysd
 from pychrono.irrlicht import ChVisualSystemIrrlicht
+from pychrono.vehicle import ChWheeledVehicleVisualSystemIrrlicht, HMMWV_Full
+import pychrono.vehicle as veh
+
 
 import pychrono as chrono
 
 from ..terrain import TTTerrain
 from pathlib import Path
 
-sys = ChSystemNSC()
-vis = ChVisualSystemIrrlicht()
-vis.AttachSystem(sys)
-vis.SetWindowSize(1024, 768)
+
+hmmwv = veh.HMMWV_Full()
+hmmwv.SetInitPosition(ChCoordsysd(ChVector3d(0,0,0.5), chrono.ChQuaterniond(1, 0, 0, 0)))
+hmmwv.Initialize()
+hmmwv.SetChassisFixed(False)
+
+hmmwv.SetChassisVisualizationType(veh.VisualizationType_PRIMITIVES)
+hmmwv.SetSuspensionVisualizationType(veh.VisualizationType_PRIMITIVES)
+hmmwv.SetSteeringVisualizationType(veh.VisualizationType_PRIMITIVES)
+hmmwv.SetWheelVisualizationType(veh.VisualizationType_PRIMITIVES)
+hmmwv.SetTireVisualizationType(veh.VisualizationType_PRIMITIVES)
+
+hmmwv.GetSystem().SetCollisionSystemType(chrono.ChCollisionSystem.Type_BULLET)
+
+vis = ChWheeledVehicleVisualSystemIrrlicht()
+vis.AttachSystem(hmmwv.GetSystem())
+vis.SetWindowSize(1280, 1024)
 vis.SetWindowTitle("Terrain Demo")
+vis.SetChaseCamera(chrono.ChVector3d(0.0, 0.0, 1.75), 6.0, 0.5)
 vis.Initialize()
-vis.AddSkyBox()
-vis.AddCamera(ChVector3d(0, 8, 6))
 vis.AddTypicalLights()
+vis.AddSkyBox()
+vis.AttachVehicle(hmmwv.GetVehicle())
 
 bmp = Path("terrain/patch_1.bmp").resolve()
-terrain = TTTerrain(sys, str(bmp))
-print(bmp.resolve())
+terrain = TTTerrain(hmmwv.GetSystem(), str(bmp))
 terrain.Initialize()
 
+step_size = 3e-3;
 
 while (vis.Run()):
+    time = hmmwv.GetSystem().GetChTime()
+
     vis.BeginScene()
     vis.Render()
     vis.EndScene()
-    sys.DoStepDynamics(0.01)
+    terrain.Advance(step_size)
+    hmmwv.Advance(step_size)
+    vis.Advance(step_size)
+
+    terrain.Synchronize(time)
+
--- src/terrain.py
+++ src/terrain.py
@@ -1,18 +1,25 @@
 from pychrono.vehicle import RigidTerrain, Patch, GetDataFile
-from pychrono.core import ChSystem, ChContactMaterialNSC,ChContactMaterial, ChCoordsysd
+from pychrono.core import ChSystem, ChContactMaterialNSC,ChContactMaterial, ChCoordsysd, ChQuaterniond, ChVector3d, ChColor
 
 import pychrono as chrono
 from pathlib import Path
+
+TERRAIN_TEXTURE  = str(Path("terrain/terrainTexture.jpg").resolve())
 
 class TTTerrain(RigidTerrain):
 
     def __init__(self, system: ChSystem, path: str, *args):
         super().__init__(system, *args)
-        patch = self.AddPatch(self.contact_material_one(), ChCoordsysd(), path, 600.0, 600.0, 0.0, 100.0)
-        patch.SetTexture(GetDataFile("terrain/textures/tile4.jpg"), 200, 200)
+        patch = self.AddPatch(
+            self.contact_material_one(),
+            ChCoordsysd(ChVector3d(0,0,0), ChQuaterniond(0,0,0,1)),
+            path, 50.0, 50.0, 0.0, 3.0
+        )
+        patch.SetColor(ChColor(1.0,0.5,0))
+        patch.SetTexture(str(Path("terrain/terrainTexture.jpg").resolve()), 6.0,6.0)
     
     def contact_material_one(self) -> ChContactMaterialNSC:
         material = ChContactMaterialNSC()
-        material.SetRollingFriction(0.2)
-        material.SetStaticFriction(0.6)
+        material.SetFriction(0.9)
+        material.SetRestitution(0.01)
         return material
